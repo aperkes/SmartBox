70b02d94176a789a7fff86f3ea0183d3fd0b3055
aperkes <perkes.ammon@gmail.com>
2019-04-05 17:58:51 -0400

    v1.01 Read to port to birdview-3, needs fine tuning
    Everything works, including the needlessly complex individual cage monitoring system
    I would like to add some diagnostics and fail conditions so that if something weird happens
    we still get good data. Here are some cases to watch out for:
    -Camera fails to get tag so it has to revert to whole motion tracking
    -One of the cameras is unplugged
    -Birds just never stop moving
    
    Most of that framework is in place, so I just need to add it in.
    But by all accounts this is looking very good, and needs some fine tuning on the system that will actually run it.

--- run_playbacks.py
+++ run_playbacks.py
@@ -9,31 +9,42 @@ import numpy as np
 import cv2
 import time
 import pdb
-import threading, Queue 
+import threading, queue 
 import pyaudio, wave
 from subprocess import Popen
+import pandas as pd
+import cv2.aruco as aruco
 
 #web_cam_ids = 
-motion_threshold = 12
-bird_threshold = 3
+motion_threshold = 10
+cage_motion = 10
+frame_motion = 12
+
+bird_threshold = 7
 codec = 'MJPG'
 out_type = '.avi'
 monitor_duration = 60
 record_duration = 10
+song_delay = 2
 n_cameras = 4
 n_cages = 4
 PLAYING = False
 
 ## Dictionary of the locations of cages in each camera. This will actually be procedurally generated using tags
-window_dict = {
-    0: [(slice(0,100),slice(0,100))],
-    1: [(slice(100,200),slice(0,100))],
-    2: [(slice(0,100),slice(100,200))],
-    3: [(slice(100,200),slice(100,200))]}
+
+## Default window dict, basically each camera tracks the entire camera
+MONITOR_CAGES = False
+default_window_dict = {
+    0: [0,(slice(0,-1),slice(0,-1))],
+    1: [1,(slice(0,-1),slice(0,-1))],
+    2: [2,(slice(0,-1),slice(0,-1))],
+    3: [3,(slice(0,-1),slice(0,-1))]}
+window_dict = dict(default_window_dict)
 ## Indexed array relating actual cages to their location within cameras. 
+
 cage_indices = np.array([[0],[1],[2],[3]])
 
-## Dictionary of identities of each camera. This will also be set using tags
+## Dictionary of identities of each camera. This will also be set using tags, might be unnecessary
 camera_dict = {
     0: 'cam0',
     1: 'cam1',
@@ -65,20 +76,167 @@ def check_cameras(caps):
     h,w = fshape[0],fshape[1]
     return h,w,fps
 
+## Find markers, and define cameras (eventually...)
+def locate_markers(caps, visual = False):
+    success_array = np.array([
+        [1,1,1,0, 1,0,1,0, 0,0,0,0],
+        [0,1,1,1, 0,1,0,1, 0,0,0,0],
+        [0,0,0,0, 1,0,1,0, 1,1,1,0],
+        [0,0,0,0, 0,1,0,1, 0,1,1,1]])
 
-def locate_markers():
-    pass
-    return locations
+    complete = False
+    n_markers = 12
+    corner_array = np.zeros([n_cameras,n_markers,2])
+    aruco_dict = aruco.Dictionary_get(aruco.DICT_6X6_250)
+    parameters = aruco.DetectorParameters_create()
+    count = 0
+    cutoff = 200
+    while complete == False and count < cutoff:
+        for c in range(len(caps)):
+            ret, frame = caps[c].read()
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            corners, ids, rejectedImgPoints = aruco.detectMarkers(gray, aruco_dict, parameters=parameters)
+            if len(corners) == 0:
+                if visual:
+                    cv2.imshow(str(c),frame)
+                    if cv2.waitKey(100) & 0xFF == ord('q'):
+                        break
+                continue
+            frame_markers = aruco.drawDetectedMarkers(frame.copy(), corners, ids)
+            if visual:
+                cv2.imshow(str(c),frame_markers)
+                if cv2.waitKey(100) & 0xFF == ord('q'):
+                    break
+            for i in ids:
+                tag_id = i[0] - 1
+                corner_index = np.where(ids == i)[0][0]
+                first_corner = tuple(corners[corner_index][0][0])
+                camera = c
+                if corner_array[c,tag_id,0] != 0:
+                    continue
+                else:
+                    corner_array[c,tag_id] = first_corner
+## Once you have a value for every expected marker for each camera, you're done.
+        if count == cutoff // 2:
+            camera_order, corner_array = get_order(corner_array)
+            #pdb.set_trace()
+            caps = [caps[int(x)] for x in camera_order] 
+        test_array = (corner_array[:,:,0] != 0)
+        if np.array_equal(test_array,success_array):
+            complete = True 
+            print('Success!!')
+        else:
+            count +=1
+    return complete, caps, corner_array
 
+def get_order(corner_array):
+    global n_cages, MONITOR_CAGES
+## Find which camera has unique indices so that I can have everything ordered nicely
+## This might be a bit overkill...and I'll need to make sure all this works. 
+    #camera 0 : where corner_array[?,0] != 00 
+    #camera 1: where corner_array[?,3] != 00
+    #camera 2: where corner_array[?,8] != 00
+    #camera 3: where corner_array[?,11] != 00
+    cam0_index = np.where(corner_array[:,0,0] != 0)[0]
+    cam1_index = np.where(corner_array[:,3,0] != 0)[0]
+    cam2_index = np.where(corner_array[:,8,0] != 0)[0]
+    cam3_index = np.where(corner_array[:,11,0] != 0)[0]
+    camera_order = [cam0_index,cam1_index,cam2_index,cam3_index]
+    for c in camera_order:
+        if len(c) > 1 or len(c) == 0:
+            MONITOR_CAGES = False
+            camera_order = np.array([0,1,2,3])
+            n_cages == 4
+
+            return camera_order, corner_array
+        else:
+            continue
+    camera_order = np.array([int(cam0_index),int(cam1_index),int(cam2_index),int(cam3_index)])
+    MONITOR_CAGES = True
+    #pdb.set_trace()
+    corner_array = corner_array[camera_order]
+## If this works, set n_cages to 9. 
+    n_cages = 9
+    return camera_order, corner_array
+     
 ## Based on markers, figure out which cage is which
-def define_cages():
-    return cage_indices
+def define_cages(corner_array):
+    global default_window_dict, window_dict, camera_dict
+## If it hasn't found all the tags correctly this will break
+    if MONITOR_CAGES == False:
+        print('Setting to default monitors (monitor all)')
+        window_dict = default_window_dict
+        return window_dict
+    else:
+        pass
+    camera_order,corner_array = get_order(corner_array)
 
+## Buckle up, things are about to get notation heavy
+## It might be better to just use the corner array notation, but I this was a little easier to wrap my head around. 
+## Check out 'corner_notation.png' for a handy reference for all this. 
+    cam0_x0,cam0_y0 = corner_array[0,0]
+    cam0_x1,cam0_x2 = corner_array[0,1,0],corner_array[0,2,0]
+    cam0_y4 = corner_array[0,4,1]
+
+    cam1_x2 = corner_array[1,2,0]
+    cam1_x3,cam1_y3 = corner_array[1,3]
+    cam1_y5 = corner_array[1,5,1]
+
+    cam2_x8,cam2_y8 = corner_array[2,8]
+    cam2_x9,cam2_x10 = corner_array[2,9,0], corner_array[2,10,0]
+    cam2_y4, cam2_y6 = corner_array[2,4,1], corner_array[2,6,1]
+
+    cam3_x10 = corner_array[3,10,0]
+    cam3_x11, cam3_y11 = corner_array[3,11]
+    cam3_y5, cam3_y7 = corner_array[3,5,1],corner_array[3,7,1]
+    
+    cam0_col0, cam0_col1 = slice(int(cam0_x0),int(cam0_x1)), slice(int(cam0_x1),int(cam0_x2))
+    cam0_row0 = slice(int(cam0_y0),int(cam0_y4))
+
+    cam1_col2 = slice(int(cam1_x2),int(cam1_x3))
+    cam1_row0 = slice(int(cam1_y3),int(cam1_y5))
+
+    cam2_col0,cam2_col1 = slice(int(cam2_x8),int(cam2_x9)),slice(int(cam2_x9),int(cam2_x10))
+    cam2_row1, cam2_row2 = slice(int(cam2_y4),int(cam2_y6)),slice(int(cam2_y6),int(cam2_y8))
+
+    cam3_col2 = slice(int(cam3_x10),int(cam3_x11))
+    cam3_row1,cam3_row2 = slice(int(cam3_y5),int(cam3_y7)),slice(int(cam3_y7),int(cam3_y11))
+
+    window_dict = {
+        0: [0, (cam0_row0, cam0_col0)],
+        1: [0, (cam0_row0, cam0_col1)],
+        2: [1, (cam1_row0, cam1_col2)],
+        3: [2, (cam2_row1, cam2_col0)],
+        4: [2, (cam2_row1, cam2_col1)],
+        5: [3, (cam3_row1, cam3_col2)],
+        6: [2, (cam2_row2, cam2_col0)],
+        7: [2, (cam2_row2, cam2_col1)],
+        8: [3, (cam3_row2, cam3_col2)]}
+    return window_dict
+
+def diagnostics(caps):
+## Function that should check the cameras and return where
+# Cameras are all working
+# Whether the tags have been located successfully
+# Related to that, what the threshold should be
+    pass
+    """ 
+The two big conditions I imagine here are: 
+a) cameras are all working, tags have all been found and everything looks great
+b) cameras are all working, tags are found, but there's some overlap, so I'm not sure which camera is which
+....b) I could get a sense based on which is further over, but that's probably not worth it.
+c) cameras are all working, but tags weren't found successfully
+d) cameras aren't working
+
+In the case of b/c, I should be looking for total movement, 
+in the case of d, I should probably shut down and send some sort of a warning message.
+    return condition
+    """
 
 ## Wait until cowbirds are calm, once it is, return True
 # If they never calm down, return false, and decide what to do. 
 def monitor_cameras(caps,visual = False):
-    global n_cameras, window_dict, cage_indices, monitor_duration, h, w
+    global n_cameras, n_cages, window_dict, cage_indices, monitor_duration, h, w
     counts = 100
     success = False
     current_frames = np.empty([n_cameras,h,w])
@@ -93,35 +251,47 @@ def monitor_cameras(caps,visual = False):
             current_frames[c] = cv2.cvtColor(color_frame,cv2.COLOR_BGR2GRAY)
     previous_frames = np.copy(current_frames)
 ## Until it exhausts its check time or is successful, within each camera, within each designated cage, check for motion
-    end_time = time.time() + monitor_duration
+    #end_time = time.time() + monitor_duration
+    end_time = time.time() + 60
     count = 0
     while time.time() < end_time and success == False:
+    #while True:
 # first read each camera, then compare
         for c in range(n_cameras):
             rets[c], color_frame = caps[c].read()
-        for c in range(n_cameras):
+        #for c in range(n_cameras):
             if rets[c] == False:
                 continue
             current_frames[c] = cv2.cvtColor(color_frame,cv2.COLOR_BGR2GRAY)
-            windows = window_dict[c]
-            for win in range(len(windows)):
-                w_pixels = windows[win]
+## I could use window_dict as a fail safe, and just convert it in the case that the cages aren't found. 
+##NOTE: This means I need to set n_cages to 4 if I can't locate the cages
+            for cage_index in range(n_cages):
+                win = window_dict[cage_index]
+                if win[0] != c:
+                    #print('skipped ' + str(c))
+                    continue
+                w_pixels = win[1]
+                #pdb.set_trace()
                 dif = cv2.absdiff(current_frames[c][w_pixels],previous_frames[c][w_pixels])
+                
                 dif[dif < 10] = 0
-                cage_index = cage_indices[c,win]
 ## Save both the amount of motion, and a boolean of whether it's above the threshold. 
                 log_sum = np.log(np.sum(dif))
                 cage_motion[count,cage_index] = log_sum
                 if log_sum < motion_threshold:
                     bool_motion[count,cage_index] = False
                 if visual:
-                    cv2.putText(dif,str(np.round(log_sum,2)),(5,5),cv2.FONT_HERSHEY_SIMPLEX,.5,255)
-                    cv2.imshow(str(cage_index),dif)
+                    cv2.putText(dif,str(np.round(log_sum,2)),(5,15),cv2.FONT_HERSHEY_SIMPLEX,.5,255)
+                    cv2.imshow('cam:' + str(c) + 'cage:' + str(cage_index),dif)
+                    #cv2.imshow('cam:' + str(c) + 'cage:' + str(cage_index),current_frames[c][w_pixels])
+                    if cv2.waitKey(1) & 0xFF == ord('q'):
+                        break
         sum_bools = np.sum(bool_motion,1)
         if max(sum_bools) <= n_cages - bird_threshold:
+            pass
+            print('success!!!')
             success = True 
-        if cv2.waitKey(100) & 0xFF == ord('q'):
-            break
+            print(sum_bools)
         count = (count + 1) % counts
         previous_frames = np.copy(current_frames)
     required_time = monitor_duration - time.time() - end_time ## probably should have just tracked start time...
@@ -130,14 +300,15 @@ def monitor_cameras(caps,visual = False):
     return success, cage_motion, required_time         
 
 def record_AV(caps, mic = None, stimulus = True):
-    q = Queue.Queue()
+    q = queue.Queue()
     playing = False
     q.put(playing)
+
+    a = threading.Thread(target=record_audio, args=(record_duration,))
+    v = threading.Thread(target=record_video, args=(caps,q))
     if stimulus:
         song = select_song()
         s = threading.Thread(target=play_wav, args=(song,q))
-    a = threading.Thread(target=record_audio, args=(record_duration,))
-    v = threading.Thread(target=record_video, args=(caps,q))
 
     print('starting audio')
     a.start()
@@ -157,7 +328,7 @@ def record_video(caps,q):
     global codec, camera_dict, n_cameras, record_duration, fps, w,h 
     global v_tmpfiles, v_start
     #h,w,fps = check_cameras(caps)
-    v_outroot = 'AV-test'
+    v_outroot = 'tmp_video'
     v_tmpfiles = []
     writers = [0] * n_cameras
     fourcc = cv2.VideoWriter_fourcc(*codec)
@@ -185,55 +356,6 @@ def record_video(caps,q):
 def add_marker(frame, c):
     frame[0:50,0:50] = [0,255,0]
     return frame
-
-## This works, but isn't necessary. If we need to record from a specific mic we can use it. 
-def record_audio_complex(length = 10, mic = None):
-    p = pyaudio.PyAudio()
-
-    if mic == None:
-        dev_index = 0
-    else:
-        dev_index = mic
-    device_info = p.get_device_info_by_index(dev_index)
-
-# Set Parameters
-    FRAMES_PERBUFF = 2048
-    FORMAT = pyaudio.paInt16
-    CHANNELS = 1
-    FRAME_RATE = int(device_info['defaultSampleRate'])
-
-# Boot up stream and populate data
-    stream = p.open(format=FORMAT, channels=CHANNELS, rate=FRAME_RATE, input=True, 
-    input_device_index=mic, frames_per_buffer=FRAMES_PERBUFF)
-    frames = []
-
-    REC_SECONDS = length
-    n_chunks = int(REC_SECONDS * FRAME_RATE / FRAMES_PERBUFF)
-    a_start = time.time()
-    for i in range(0, n_chunks):
-        data = stream.read(FRAMES_PERBUFF)
-        frames.append(data)
-    a_stop = time.time()
-    stream.stop_stream()
-    stream.close()
-    p.terminate()
-
-    date_string = time.strftime("%Y-%m-%d-%H:%M")
-    a_outfile = 'test_audio_' + date_string + '.wav'
-
-    wf = wave.open(a_outfile, 'wb')
-    wf.setnchannels(CHANNELS)
-    wf.setsampwidth(p.get_sample_size(FORMAT))
-    wf.setframerate(FRAME_RATE)
-    wf.writeframes(b''.join(frames))
-    wf.close()
-
-    global A_START,A_FINISH, A_TMP
-    A_START = a_start
-    A_FINISH = a_stop
-    A_TMP = a_outfile
-    print('finished audio recording!')
-    return a_start, a_stop, a_outfile
 
 def record_audio(length = 10):
     global A_START, A_STOP, A_TMP
@@ -267,46 +389,23 @@ def splice_AV(v_files = None,a_file = None,offset = None):
         av_outfiles.append(av_outfile)
     return av_outfiles
 
-## This isn't needed, and doesn't work. If we ever want to play from a specific speaker, we can revisit it
-def play_wav_complex(p_audio = None, wav_file = 'ND_right.wav', out_index = 2):
-    CHUNK = 1024
-    if p_audio == None:
-        p = pyaudio.PyAudio()
-    else:
-        p = p_audio
-    wf = wave.open(wav_file, 'rb')
-    dev_count = p.get_device_count()
-    for x in range(dev_count):
-        print(p.get_device_info_by_index(x))
-
-    stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
-                    channels=wf.getnchannels(),
-                    rate=wf.getframerate(),
-                    output=True,
-                    output_device_index = out_index)
-    data = wf.readframes(CHUNK)
-    global PLAYING
-    PLAYING = True
-    print(len(data))
-    while len(data) > 0:
-        stream.write(data)
-        data = wf.readframes(CHUNK)
-
-    stream.stop_stream()
-    stream.close()
-    PLAYING = False
-    print('finished playing sound!')
-
 def play_wav(wav_file,q):
+    time.sleep(1)
     playing = True
     q.put(playing)
-    p_proc = Popen(['aplay','./ND_left.wav'])
+    p_proc = Popen(['aplay',wav_file])
     p_proc.wait()
     playing = False
     q.put(playing)
 
 def select_song():
-    song = './ND_right.wav'
+    song_log = './2019_log.csv'
+    song_directory = './Songs/'
+    song_df = pd.read_csv(song_log)
+    next_row = song_df[song_df['Played'] == 0].iloc[0]
+    song = next_row['SongFile']
+    #song = './ND_right.wav'
+    song = song_directory + song
     return song
 
 def unload_cameras(caps):
@@ -322,6 +421,13 @@ def update_log():
 
 if __name__ == "__main__":
     caps = load_cameras()
+    h,w,fps = check_cameras(caps)
+    complete, caps, corner_array = locate_markers(caps, visual=False)
+    #print(complete, corner_array)
+    if complete:
+        camera_order, corner_array = get_order(corner_array)
+        define_cages(corner_array)
+        #print(window_dict)
     success, cage_motion,monitor_time = monitor_cameras(caps,visual=False)
     #unload_cameras(caps)
     #caps = load_cameras()

436d41cccdb203cef804fc940a9c224d2e59034c
aperkes <perkes.ammon@gmail.com>
2019-04-08 18:22:06 -0400

    v1.3 more housekeeping

--- Export to Windows/run.bat
+++
@@ -1,9 +0,0 @@
-"C:\Program Files\iSpy\iSpy.exe" commands "allon"
-timeout 10 >nul
-"C:\Program Files\iSpy\iSpy.exe" commands "record"
-timeout 5 >nul
-python MonoWindows.py
-timeout 10 >nul
-"C:\Program Files\iSpy\iSpy.exe" commands "recordstop"
-timeout 10 >nul
-"C:\Program Files\iSpy\iSpy.exe" commands "alloff"
